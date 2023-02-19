1d0eb1efd1e4a6b33218c581737dab5144972380
c011825019 <c011825019@gmail.com>
2023-02-19 16:07:11 +0900

    Add SubActivity

--- app/src/main/java/com/example/myfirstandroidjava/MainActivity.java
+++ app/src/main/java/com/example/myfirstandroidjava/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.content.Intent;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -29,5 +30,13 @@ public class MainActivity extends AppCompatActivity {
                 textView.setText(text);
             }
         });
+    }
+
+    public void onClick(View v) {
+        Intent intent = new Intent(this, SubActivity.class);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
